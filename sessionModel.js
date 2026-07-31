5705c8d0dbcd56dace78b5c29c7102f691073be7
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-31 14:46:14 +0500

    Create sessionModel.js

---
+++ sessionModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+  key: { type: String, unique: true },
+  data: { type: Object }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Session', sessionSchema);
